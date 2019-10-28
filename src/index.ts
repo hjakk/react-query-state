31de040837c81dcc3f8d7f99be5954d12c68fd1d
somebody <se.ev@ya.ru>
2019-10-28 12:15:29 +0300

    update

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,5 @@
 import * as React from 'react'
-
-
-function stringify(data: any, prevKey?: string | null): string {
-  let _q = ''
-  for (const key in data) {
-    if (!data.hasOwnProperty(key)) continue
-
-    const _v: any = data[key]
-    const _k: string = prevKey ? `${ prevKey }[${ Array.isArray(data) ? '' : key }]` : key
-    if (_v !== undefined && _v !== null && _v !== '') {
-      if (_q) _q += '&'
-      if (typeof _v === 'object') {
-        _q += stringify(_v, `${ _k }`)
-      }
-      else {
-        _q += encodeURIComponent(_k)
-        _q += `=${ encodeURIComponent(_v) }`
-      }
-    }
-  }
-  return _q
-}
-
-function parse(str: string): { [key: string]: any } {
-  str = str.replace(/^\?/, '')
-  const params: any = {}
-  const arr: string[] = str.split('&')
-  let i = -1
-  while (++i < arr.length) {
-    let [_key, _value] = arr[i].split('=')
-    _key = decodeURIComponent(_key)
-    _value = decodeURIComponent(_value)
-    if (/\[\]$|\[(\d+)\]$/.test(_key)) {
-      const _rootKey: string = _key.replace(/\[\]$|\[(\d+)\]$/, '')
-      if (!params[_rootKey]) params[_rootKey] = []
-      const index: string[] | null = _key.match(/\[(\d+)\]/)
-      if (index) params[_rootKey][index[1]] = _value
-      else params[_rootKey].push(_value)
-    }
-    else if (/\[(.+)\]$/.test(_key)) {
-      const _rootKey: string = _key.replace(/\[(.+)\]$/, '')
-      if (!params[_rootKey]) params[_rootKey] = {}
-      const _k: string[] | null = _key.match(/\[(.+)\]$/)
-      if (_k) params[_rootKey][_k[1]] = _value
-    }
-    else {
-      params[_key] = _value
-    }
-  }
-  return params
-}
-
+import _query from '@hjk/query'
 
 
 const hasProp = Object.prototype.hasOwnProperty
@@ -87,7 +36,9 @@ function addListener(this: any, props: any): void {
 function handleRouterParams(this: any, { history, location }: any): void {
   if (!hasProp.call(this.history, 'push')) {
     this.history.push = history.push
-    if (location) mapState(parse(location.search), this.state)
+    if (location) mapState(_query.parse(location.search), this.state)
+    const _search = _query.stringify(this.state)
+    if (!location || location.search !== _search) history.push({ search: _search })
   }
 }
 
@@ -102,7 +53,7 @@ export default function createUrlState(defaultState: any): any {
     clearTimeout(history.timer)
     if (history.push) {
       history.timer = setTimeout(() => {
-        history.push({ search: stringify(state) })
+        history.push({ search: _query.stringify(state) })
       }, 1000)
     }
   }
